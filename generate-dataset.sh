a768dd44a6139b530d8abe8d26060c15496746ea
Ivan Veselov <veselov.iva@gmail.com>
2019-01-11 10:56:26 +0300

    Change gradle output style to plain.

--- generate-dataset.sh
+++ generate-dataset.sh
@@ -7,5 +7,5 @@ fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )" # from https://stackoverflow.com/a/246128
 
-$DIR/gradlew -p $DIR runDatasetGeneration -PprojectFolder="$PWD/$1" -PoutputDir="$PWD/$2"
+$DIR/gradlew --console=plain -p $DIR runDatasetGeneration -PprojectFolder="$PWD/$1" -PoutputDir="$PWD/$2"
 
